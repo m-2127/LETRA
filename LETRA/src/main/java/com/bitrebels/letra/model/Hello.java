a6c530834215e5873259381ab0c87c4cf863e2ed
Shafi <44125051+m-2127@users.noreply.github.com>
2019-07-04 13:36:14 +0530

    added email and hello class for testing

---
+++ LETRA/src/main/java/com/bitrebels/letra/model/Hello.java
@@ -0,0 +1,20 @@
+package com.bitrebels.letra.model;
+
+public class Hello {
+    private String greeting;
+
+    public Hello() {
+    }
+
+    public Hello(String greeting) {
+        this.greeting = greeting;
+    }
+
+    public String getGreeting() {
+        return greeting;
+    }
+
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
+    }
+}
