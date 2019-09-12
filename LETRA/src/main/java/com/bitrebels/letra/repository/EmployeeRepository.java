4e8489b90d5cfb30a19b5869d730e1226e44031f
Shafi <44125051+m-2127@users.noreply.github.com>
2019-09-12 13:23:45 +0530

    method added to query employees based on the project

--- LETRA/src/main/java/com/bitrebels/letra/repository/EmployeeRepository.java
+++ LETRA/src/main/java/com/bitrebels/letra/repository/EmployeeRepository.java
@@ -1,9 +1,12 @@
 package com.bitrebels.letra.repository;
 
+import com.bitrebels.letra.model.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bitrebels.letra.model.Employee;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+import java.util.List;
 
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    List<Employee> findByProject(Project project);
 }
