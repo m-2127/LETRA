799f0d25da33f13db3b2df403c18247a2ff17ee6
Shafi <44125051+m-2127@users.noreply.github.com>
2019-09-14 16:42:38 +0530

    method created to extract leaves based on rm, employee, start date and end date

--- LETRA/src/main/java/com/bitrebels/letra/repository/LeaveRepo.java
+++ LETRA/src/main/java/com/bitrebels/letra/repository/LeaveRepo.java
@@ -1,11 +1,18 @@
 package com.bitrebels.letra.repository;
 
-import com.bitrebels.letra.model.Leave;
-import com.bitrebels.letra.model.LeaveRequest;
+import com.bitrebels.letra.model.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
-public interface LeaveRepo extends JpaRepository<Leave, LeaveRequest> {
+public interface LeaveRepo extends JpaRepository<Leave, Long> {
 //    int countByStartDateAfterAndFinishDateBefore(LocalDate startDate, LocalDate currentDate);
+//    List<Leave> findByEmployeeAndDatesBetween(Employee employee, LocalDate startDate, LocalDate endDate);
+//    int countLeavesByDatesBetween(LeaveDates startDate, LeaveDates endDate);
+//    List<Leave> findByLeaveDates_Date(LocalDate localDate);
+    List<Leave> findByLeaveDates_DateBetweenAndEmployeeAndReportingManager(
+            LocalDate startDate, LocalDate endDate, Employee employee , ReportingManager rm);
 }
