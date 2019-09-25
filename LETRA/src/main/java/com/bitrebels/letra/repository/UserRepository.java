94d7c55a0535a17e16c9ffb4fc8146fe8a513a27
Shafi <44125051+m-2127@users.noreply.github.com>
2019-09-25 21:03:40 +0530

    query to find users who are not managers

--- LETRA/src/main/java/com/bitrebels/letra/repository/UserRepository.java
+++ LETRA/src/main/java/com/bitrebels/letra/repository/UserRepository.java
@@ -1,17 +1,25 @@
 package com.bitrebels.letra.repository;
 
+import java.util.List;
 import java.util.Optional;
 
+import com.bitrebels.letra.model.ReportingManager;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.bitrebels.letra.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
     Optional<User> findByEmail(String email);
     Boolean existsByMobilenumber(String mobilenumber);
     Boolean existsByEmail(String email);
     Optional<User> findByResetToken(String resetToken);
-    
+
+//    @Query("SELECT u FROM User u left outer join u.hrManager m on u.hrManager = m.hrmId where m.hrmId is null ")
+    @Query("SELECT u FROM User u  where not exists(select h from u.hrManager h where u.id= h.hrmId)")
+    List<User> findByNonManagers();
+
 }
