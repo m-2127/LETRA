313993c39b2407cc919a98b750893f18e96d95ef
Shafi <44125051+m-2127@users.noreply.github.com>
2019-10-02 20:14:41 +0530

    method added to retrieve progress

--- LETRA/src/main/java/com/bitrebels/letra/repository/ProgressRepo.java
+++ LETRA/src/main/java/com/bitrebels/letra/repository/ProgressRepo.java
@@ -1,14 +1,17 @@
 package com.bitrebels.letra.repository;
 
+import com.bitrebels.letra.model.LeaveRequest;
 import com.bitrebels.letra.model.Progress;
 import com.bitrebels.letra.model.ReportingManager;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Set;
 
 @Repository
 public interface ProgressRepo extends JpaRepository<Progress , Long> {
 
     List<Progress> findProgressByManager(ReportingManager reportingManager);
+    Set<Progress> findProgressByLeaveRequestAndManager(LeaveRequest leaveRequest , ReportingManager manager);
 }
