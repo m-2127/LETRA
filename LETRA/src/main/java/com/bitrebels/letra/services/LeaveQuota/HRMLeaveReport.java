c1b18d24ac70315e911c7422be759a8c8556342e
Shafi <44125051+m-2127@users.noreply.github.com>
2019-11-04 23:43:09 +0530

    HRM Leave Report

---
+++ LETRA/src/main/java/com/bitrebels/letra/services/LeaveQuota/HRMLeaveReport.java
@@ -0,0 +1,81 @@
+package com.bitrebels.letra.services.LeaveQuota;
+
+import com.bitrebels.letra.model.Employee;
+import com.bitrebels.letra.model.Leave;
+import com.bitrebels.letra.model.ReportingManager;
+import com.bitrebels.letra.repository.LeaveRepo;
+import org.apache.tomcat.jni.Local;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+@Service
+public class HRMLeaveReport {
+
+    @Autowired
+    LeaveRepo leaveRepo;
+
+    public Set<Leave> selectLeaves(String employeeString, String projectString , LocalDate startDate,
+                                    LocalDate endDate, Employee employee , ReportingManager rm){
+
+        Set<Leave> leaveSet;
+
+            if(employeeString.equalsIgnoreCase("null") && projectString.equalsIgnoreCase("null")){
+                leaveSet = leaveRepo.findByLeaveDates_DateBetween(startDate, endDate);
+            }
+            else if(employeeString.equalsIgnoreCase("null")){
+                leaveSet = leaveRepo.findByLeaveDates_DateBetweenAndReportingManager(startDate,endDate,rm);
+            }
+            else if(projectString.equalsIgnoreCase("null")){
+                leaveSet = leaveRepo.findByLeaveDates_DateBetweenAndEmployee(startDate,endDate,employee);
+            }
+            else{
+                leaveSet = leaveRepo.findByLeaveDates_DateBetweenAndEmployeeAndReportingManager(
+                        startDate,endDate,employee,rm);
+            }
+
+        return leaveSet;
+    }
+
+    public Map<String, Integer> addLeave(Set<Leave> leaveSet){
+
+        int annual=0,casual=0,sick=0,nopay=0;
+
+        Map<String , Integer> leaveReportMap = new HashMap<>();
+
+        Iterator<Leave> leaveIterator = leaveSet.iterator();
+
+        while(leaveIterator.hasNext()) {
+            Leave currentLeave = leaveIterator.next();
+            String leaveType = currentLeave.getLeaveType();
+
+                if(leaveType.equalsIgnoreCase("annual")){
+                    annual += currentLeave.getDuration();
+                }
+                else if(leaveType.equalsIgnoreCase("casual")){
+                    casual += currentLeave.getDuration();
+                }
+                else if(leaveType.equalsIgnoreCase("sick")){
+                    sick += currentLeave.getDuration();
+                }
+                else if(leaveType.equalsIgnoreCase("nopay")){
+                    nopay += currentLeave.getDuration();
+                }
+                else{
+                    continue;
+                }
+        }
+
+        leaveReportMap.put("annual" , annual);
+        leaveReportMap.put("casual" , casual);
+        leaveReportMap.put("nopay" , nopay);
+        leaveReportMap.put("sick" , sick);
+
+        return leaveReportMap;
+    }
+}
