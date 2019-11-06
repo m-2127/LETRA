b37a7c29bafa6cfe06e0f4a722b7848407560cf9
Shafi <44125051+m-2127@users.noreply.github.com>
2019-11-06 16:37:27 +0530

    resetpassword

--- LETRA/src/main/java/com/bitrebels/letra/services/ResetPassword.java
+++ LETRA/src/main/java/com/bitrebels/letra/services/ResetPassword.java
@@ -1,9 +1,12 @@
 package com.bitrebels.letra.services;
 
+import com.bitrebels.letra.message.response.ResponseMessage;
 import com.bitrebels.letra.model.User;
 import com.bitrebels.letra.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -24,8 +27,19 @@ public class ResetPassword {
 
 
 
-    public void setNewPassword(String password, User user) {
-        user.setPassword(passwordEncoder().encode(password));
+    public ResponseEntity<?> setNewPassword(String password, User user) {
+
+        boolean passmatch = passwordEncoder().matches(password,user.getPassword());
+
+        if(passmatch){
+            user.setPassword(passwordEncoder().encode(password));
             userService.save(user);
+
+            return new ResponseEntity<>(new ResponseMessage("Password changed successfully"), HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>(new ResponseMessage("The old password did not match"), HttpStatus.OK);
+        }
+
     }
 }
